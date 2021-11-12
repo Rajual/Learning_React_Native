68a8255a7b0b855c2c99e9f18e115f085f2fb821
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-11-12 16:26:40 -0500

    Add positiveNegative() funtion to '+/-' button on Calculator

--- Calculator/src/screens/CalculatorScreens.tsx
+++ Calculator/src/screens/CalculatorScreens.tsx
@@ -4,57 +4,65 @@ import { ButtonCalculator } from '../components/ButtonCalculator';
 import { styles } from '../themes/appTheme';
 
 export const CalculatorScreens = () => {
-   
+
    const [smallNumber, setSmallNumber] = useState('0')
    const [numero, setNumero] = useState('0')
-   
-   const clean=()=>{
+
+   const clean = () => {
       setNumero('0');
    }
 
-   const numerAseembler=(textNumber:string)=>{
-setNumero(numero+textNumber);
+   const numerAseembler = (textNumber: string) => {
+      setNumero(numero + textNumber);
+   }
+
+   const positiveNegative=()=>{
+      if(numero.includes('-')){
+         setNumero( numero.replace('-',''));
+      }else{
+         setNumero('-'+numero)
+      }
    }
 
    return (
-        <View style={styles.calculatorContainer}>
-            <Text style={styles.smallResult}>{smallNumber}</Text>
-            <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>{numero}</Text>
-            {/*Button row*/}
-            <View style={styles.row}>
-               <ButtonCalculator text="C" onPress={clean}/>
-               <ButtonCalculator text="+/-" onPress={clean}/>
-               <ButtonCalculator text="Del" onPress={clean}/>
-               <ButtonCalculator text="/"color="#FF9427" onPress={clean}/>
-            </View>
-            {/*Button row*/}
-            <View style={styles.row}>
-               <ButtonCalculator text="7" onPress={numerAseembler}/>
-               <ButtonCalculator text="8" onPress={numerAseembler}/>
-               <ButtonCalculator text="9" onPress={numerAseembler}/>
-               <ButtonCalculator text="X"color="#FF9427" onPress={clean}/>
-            </View>
-            {/*Button row*/}
-            <View style={styles.row}>
-               <ButtonCalculator text="5" onPress={numerAseembler}/>
-               <ButtonCalculator text="5" onPress={numerAseembler}/>
-               <ButtonCalculator text="6" onPress={numerAseembler}/>
-               <ButtonCalculator text="-"color="#FF9427" onPress={clean}/>
-            </View>
-            {/*Button row*/}
-            <View style={styles.row}>
-               <ButtonCalculator text="1" onPress={numerAseembler}/>
-               <ButtonCalculator text="2" onPress={numerAseembler}/>
-               <ButtonCalculator text="3" onPress={numerAseembler}/>
-               <ButtonCalculator text="+"color="#FF9427" onPress={clean}/>
-            </View>
-            {/*Button row*/}
-            <View style={styles.row}>
-               <ButtonCalculator text="0" expanded onPress={numerAseembler}/>
-               <ButtonCalculator text="." onPress={numerAseembler}/>
-               <ButtonCalculator text="="color="#FF9427" onPress={clean}/>
-            </View>
-            
-        </View>
-    )
+      <View style={styles.calculatorContainer}>
+         <Text style={styles.smallResult}>{smallNumber}</Text>
+         <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>{numero}</Text>
+         {/*Button row*/}
+         <View style={styles.row}>
+            <ButtonCalculator text="C" onPress={clean} />
+            <ButtonCalculator text="+/-" onPress={positiveNegative} />
+            <ButtonCalculator text="Del" onPress={clean} />
+            <ButtonCalculator text="/" color="#FF9427" onPress={clean} />
+         </View>
+         {/*Button row*/}
+         <View style={styles.row}>
+            <ButtonCalculator text="7" onPress={numerAseembler} />
+            <ButtonCalculator text="8" onPress={numerAseembler} />
+            <ButtonCalculator text="9" onPress={numerAseembler} />
+            <ButtonCalculator text="X" color="#FF9427" onPress={clean} />
+         </View>
+         {/*Button row*/}
+         <View style={styles.row}>
+            <ButtonCalculator text="5" onPress={numerAseembler} />
+            <ButtonCalculator text="5" onPress={numerAseembler} />
+            <ButtonCalculator text="6" onPress={numerAseembler} />
+            <ButtonCalculator text="-" color="#FF9427" onPress={clean} />
+         </View>
+         {/*Button row*/}
+         <View style={styles.row}>
+            <ButtonCalculator text="1" onPress={numerAseembler} />
+            <ButtonCalculator text="2" onPress={numerAseembler} />
+            <ButtonCalculator text="3" onPress={numerAseembler} />
+            <ButtonCalculator text="+" color="#FF9427" onPress={clean} />
+         </View>
+         {/*Button row*/}
+         <View style={styles.row}>
+            <ButtonCalculator text="0" expanded onPress={numerAseembler} />
+            <ButtonCalculator text="." onPress={numerAseembler} />
+            <ButtonCalculator text="=" color="#FF9427" onPress={clean} />
+         </View>
+
+      </View>
+   )
 }
